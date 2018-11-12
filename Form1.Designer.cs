83d9aa9aca200685ac93cdbc34f6739bc48aa99b
Sneha <skoman3@uic.edu>
2018-11-12 17:00:30 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace email
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.to = new System.Windows.Forms.Label();
+            this.from = new System.Windows.Forms.Label();
+            this.smtp = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.body = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(172, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(172, 124);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(172, 163);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(172, 193);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // to
+            // 
+            this.to.AutoSize = true;
+            this.to.Location = new System.Drawing.Point(150, 51);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(16, 13);
+            this.to.TabIndex = 9;
+            this.to.Text = "to";
+            // 
+            // from
+            // 
+            this.from.AutoSize = true;
+            this.from.Location = new System.Drawing.Point(139, 15);
+            this.from.Name = "from";
+            this.from.Size = new System.Drawing.Size(27, 13);
+            this.from.TabIndex = 10;
+            this.from.Text = "from";
+            // 
+            // smtp
+            // 
+            this.smtp.AutoSize = true;
+            this.smtp.Location = new System.Drawing.Point(134, 127);
+            this.smtp.Name = "smtp";
+            this.smtp.Size = new System.Drawing.Size(32, 13);
+            this.smtp.TabIndex = 11;
+            this.smtp.Text = "smtp ";
+            // 
+            // subject
+            // 
+            this.subject.AutoSize = true;
+            this.subject.Location = new System.Drawing.Point(125, 90);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(41, 13);
+            this.subject.TabIndex = 12;
+            this.subject.Text = "subject";
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(118, 163);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(53, 13);
+            this.username.TabIndex = 13;
+            this.username.Text = "username";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(114, 196);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(52, 13);
+            this.password.TabIndex = 14;
+            this.password.Text = "password";
+            // 
+            // body
+            // 
+            this.body.AutoSize = true;
+            this.body.Location = new System.Drawing.Point(22, 58);
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(24, 13);
+            this.body.TabIndex = 15;
+            this.body.Text = "key";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.smtp);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label to;
+        private System.Windows.Forms.Label from;
+        private System.Windows.Forms.Label smtp;
+        private System.Windows.Forms.Label subject;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label body;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+using System.Security.Cryptography;
+
+
+
+
+namespace email
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                MailMessage msg = new MailMessage();
+                SmtpClient client = new SmtpClient();
+                msg.From = new MailAddress("cryptocloudsneha0109@gmail.com");
+                msg.To.Add(new MailAddress("abc@gmail.com"));
+                msg.Subject = subject.Text;
+                msg.Body = richTextBox1.Text;
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("cryptocloudsneha0109@gmail.com", "cryptocloud0109");
+               
+                client.Send(msg);
+                MessageBox.Show("message sent");
+
+
+
+             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
